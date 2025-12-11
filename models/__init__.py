d965ca2f0f8d1071171dcd412eaa61cea3c0d741
yuuri-mr <k24135kk@aitech.ac.jp>
2025-12-11 22:01:19 +0900

    全体を修正
    主に、定義のエラー修正。Student→User
    動作確認

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,14 @@
-from peewee import SqliteDatabase
 from .db import db
-from .user import User        # 学籍番号, 性別, 学年
-from .job import Job          # 職種, 仕事内容
-from .registration import Registration  # user選択, job選択, 時給
+from .user import User
+from .job import Job
+from .registration import Registration
 
-# Peewee で作るテーブル一覧
 MODELS = [
     User,
     Job,
     Registration,
 ]
 
-# DB初期化
 def initialize_database():
     db.connect()
     db.create_tables(MODELS, safe=True)
--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,6 @@ from .user import user_bp
 from .job import job_bp
 from .registration import registration_bp
 
-# Blueprint一覧
 blueprints = [
     user_bp,
     job_bp,
